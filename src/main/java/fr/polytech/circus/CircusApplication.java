a904fc039db9526cabc1b18366ed57598de86241
skatos-ux <nicolas.magne1@outlook.fr>
2021-11-21 17:55:58 +0100

    Tabs

--- src/main/java/fr/polytech/circus/CircusApplication.java
+++ src/main/java/fr/polytech/circus/CircusApplication.java
@@ -8,16 +8,16 @@ import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application
+public class CircusApplication extends Application
 	{
 	@Override
 	public void start ( Stage stage ) throws IOException
 		{
-		FXMLLoader fxmlLoader = new FXMLLoader ( HelloApplication.class.getResource ( "hello-view.fxml" ) );
-		Scene      scene      = new Scene ( fxmlLoader.load (), 320, 240 );
+		FXMLLoader fxmlLoader = new FXMLLoader ( CircusApplication.class.getResource ( "main_window.fxml" ) );
+		Scene      scene      = new Scene ( fxmlLoader.load (), 1000, 500 );
 
 		scene.getStylesheets().add ( BootstrapFX.bootstrapFXStylesheet () );
-		stage.setTitle ( "Hello!" );
+		stage.setTitle ( "Application Circus" );
 		stage.setScene ( scene );
 		stage.show ();
 		}
--- src/main/java/fr/polytech/circus/MainWindow.java
+++ src/main/java/fr/polytech/circus/MainWindow.java
@@ -3,7 +3,7 @@ package fr.polytech.circus;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController
+public class MainWindow
 	{
 	@FXML
 	private Label welcomeText;
