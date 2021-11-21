b7960fa12e6d33920bd21231f8b1895ec8296477
skatos-ux <nicolas.magne1@outlook.fr>
2021-11-21 14:28:01 +0100

    Initial commit

---
+++ src/main/java/fr/polytech/circus/HelloController.java
@@ -0,0 +1,16 @@
+package fr.polytech.circus;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController
+	{
+	@FXML
+	private Label welcomeText;
+
+	@FXML
+	protected void onHelloButtonClick ()
+		{
+		welcomeText.setText ( "Welcome to JavaFX Application!" );
+		}
+	}
