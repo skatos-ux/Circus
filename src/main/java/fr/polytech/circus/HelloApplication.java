71b9e768ca3d0b2fbf97913b1bb9319ac57d01a4
skatos-ux <nicolas.magne1@outlook.fr>
2021-11-21 17:02:28 +0100

    BootstrapFX exemple

--- src/main/java/fr/polytech/circus/HelloApplication.java
+++ src/main/java/fr/polytech/circus/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -14,6 +15,8 @@ public class HelloApplication extends Application
 		{
 		FXMLLoader fxmlLoader = new FXMLLoader ( HelloApplication.class.getResource ( "hello-view.fxml" ) );
 		Scene      scene      = new Scene ( fxmlLoader.load (), 320, 240 );
+
+		scene.getStylesheets().add ( BootstrapFX.bootstrapFXStylesheet () );
 		stage.setTitle ( "Hello!" );
 		stage.setScene ( scene );
 		stage.show ();
