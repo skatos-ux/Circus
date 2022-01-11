4a1c282fa63ba20e393bd3f93d6b9bfad10aabe1
JackZUEG <milanpsqr@gmail.com>
2022-01-11 12:42:40 +0100

    Classes de test

---
+++ src/test/java/fr/polytech/circus/model/MetaSequenceTest.java
@@ -0,0 +1,53 @@
+package fr.polytech.circus.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetaSequenceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getDuration() {
+    }
+
+    @Test
+    void setDuration() {
+    }
+
+    @Test
+    void getListSequences() {
+    }
+
+    @Test
+    void setListSequences() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void addSequence() {
+    }
+
+    @Test
+    void remSequence() {
+    }
+}
