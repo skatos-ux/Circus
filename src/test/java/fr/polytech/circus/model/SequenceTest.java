4d0f51ebb4cab9c1bd0e2bb4a2a97f9167665fbc
chuckloris <perdereau.loris@orange.fr>
2022-01-13 22:08:28 +0100

    Ajouts de Mariem sans la suppression des fichiers du .idea

---
+++ src/test/java/fr/polytech/circus/model/SequenceTest.java
@@ -0,0 +1,45 @@
+package fr.polytech.circus.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SequenceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getName() {}
+
+    @Test
+    void setName() {}
+
+    @Test
+    void getDuration() {}
+
+    @Test
+    void setDuration() {}
+
+    @Test
+    void getListMedias() {
+    }
+
+    @Test
+    void setListMedias() {}
+
+
+    @Test
+    void addMedia() {}
+
+    @Test
+    void remMedia() {}
+
+    @Test
+    void testToString() {}
+}
