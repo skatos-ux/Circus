1564a801071551fa8e966d08b0c0c0befe4f33df
Mariem-abdelmalik <19059@esp.mr>
2022-01-13 19:33:14 +0100

    exportation en pdf

---
+++ src/test/java/fr/polytech/circus/model/SequenceTest.java
@@ -0,0 +1,46 @@
+package fr.polytech.circus.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SequenceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+    
+    @Test
+    public void getName() {}
+
+    @Test
+    public void setName() {}
+
+    @Test
+    public void getDuration() {}
+
+    @Test
+    public void setDuration() {}
+
+    @Test
+    public void getListMedias() {
+    }
+
+    @Test
+    public void setListMedias() {}
+
+
+    @Test
+    public void addMedia() {}
+
+    @Test
+    public void remMedia() {}
+
+    @Test
+    public void toString() {  }
+}
+}
