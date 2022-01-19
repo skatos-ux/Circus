57c3dfb59a0493378c95ec3f9ba9f2fc87d89b06
chuckloris <perdereau.loris@orange.fr>
2022-01-20 00:05:47 +0100

    Légers changements dans la prise en compte de la méta séquence et dans le nom de la fonction pour la jouer.

--- src/main/java/fr/polytech/circus/controller/ViewerController.java
+++ src/main/java/fr/polytech/circus/controller/ViewerController.java
@@ -86,6 +86,7 @@ public class ViewerController
 		}
 
 		this.playingMetaSequence = metaSequence;
+		startMetaSequence(playingMetaSequence);
 	}
 
 	//******************************************************************************************************************
@@ -204,7 +205,7 @@ public class ViewerController
 	/**
 	 * Affiche la méta-séquence donnée en paramètre
 	 */
-	@FXML private void showMetaseq(MetaSequence metaSequence)
+	@FXML private void startMetaSequence(MetaSequence metaSequence)
 	{
 		// Pour chaque séquence de la méta séquence
 		for (Sequence sequence : metaSequence.getListSequences())
@@ -273,12 +274,6 @@ public class ViewerController
 
 			}
 		}
-		/*
-		mediaPlayer = new MediaPlayer(media);
-		mediaPlayer.setAutoPlay(true);
-		mediaView.setMediaPlayer(mediaPlayer);
-		*/
-
 	}
 
 	/**
