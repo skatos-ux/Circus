f1f7534083905adee6e56df09bf6a4b18c0092dc
chuckloris <perdereau.loris@orange.fr>
2022-01-19 23:53:41 +0100

    Fonction dans le viewer pour prendre en compte la méta-séquence donnée créée. Pour le moment les durées des médias sont gérés avec un TimeUnit.SECONDS.sleep.

--- src/main/java/fr/polytech/circus/controller/ViewerController.java
+++ src/main/java/fr/polytech/circus/controller/ViewerController.java
@@ -3,6 +3,7 @@ package fr.polytech.circus.controller;
 import fr.polytech.circus.CircusApplication;
 import fr.polytech.circus.model.MetaSequence;
 import fr.polytech.circus.model.Sequence;
+import fr.polytech.circus.model.TypeMedia;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -24,6 +25,7 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.EventListener;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Controleur permettant la gestion de modification d'une sequence
@@ -115,12 +117,57 @@ public class ViewerController
 	}
 
 	/**
+	 * Affiche le media dont le nom est donné en paramètre
+	 */
+	@FXML private void showMediaFromName(String name)
+	{
+		File mediaFile = new File("medias/" + name);
+		try
+		{
+			Media media = new Media(mediaFile.toURI().toURL().toString());
+			showMedia(media);
+		}
+		// Si l'URL est malformée, on le signale
+		catch (MalformedURLException error)
+		{
+			System.out.println("URL malformée, le chemin vers la vidéo est incorrect.");
+		}
+
+	}
+	/**
+	 * Retire la vidéo affichée
+	 */
+	@FXML private void removeMedia()
+	{
+		mediaView.setMediaPlayer(null);
+	}
+
+	/**
 	 * Affiche l'image donnée en paramètre
 	 */
 	@FXML private void showImage(Image image)
 	{
 		imageView.setImage(image);
 		imageView.setCache(true);
+	}
+
+	/**
+	 * Affiche l'image dont le nom est donné en paramètre
+	 */
+	@FXML private void showImageFromName(String name)
+	{
+		// On essaye de créer un InputStream avec le chemin du fichier
+		try
+		{
+			InputStream is = new FileInputStream("medias/" + name);
+			Image image = new Image(is);
+			showImage(image);
+		}
+		// Si le chemin n'est pas trouvé on le signale
+		catch (FileNotFoundException error)
+		{
+			System.out.println("Le fichier n'existe pas.");
+		}
 	}
 
 	/**
@@ -136,7 +183,7 @@ public class ViewerController
 	 */
 	@FXML private void testMedia() throws MalformedURLException
 	{
-		File mediaFile = new File("D:/Codezone/POLYTECH/DI5/SI/CIRCUS/medias/media-europark.png");
+		File mediaFile = new File("C:/Users/Loris/Download/four.mp4");
 		Media media = new Media(mediaFile.toURI().toURL().toString());
 		showMedia(media);
 	}
@@ -152,6 +199,86 @@ public class ViewerController
 		InputStream is = new FileInputStream("medias/test.png");
 		Image image = new Image(is);
 		showImage(image);
+	}
+
+	/**
+	 * Affiche la méta-séquence donnée en paramètre
+	 */
+	@FXML private void showMetaseq(MetaSequence metaSequence)
+	{
+		// Pour chaque séquence de la méta séquence
+		for (Sequence sequence : metaSequence.getListSequences())
+		{
+			// Pour chaque Média de la séquence
+			for (fr.polytech.circus.model.Media media : sequence.getListMedias())
+			{
+				// Si le média est une image
+				if (media.getType() == TypeMedia.PICTURE)
+				{
+					// On cache la vidéo s'il y en avait une
+					removeMedia();
+
+					// On affiche l'image
+					showImageFromName(media.getName());
+					// On attend pendant la duration du Media
+
+					// wait x ?
+					try
+					{
+						TimeUnit.SECONDS.sleep(media.getDuration().getSeconds());
+					}
+					// Si l'attente est interrompue on le signale
+					catch (InterruptedException error)
+					{
+						System.out.println("Interruption de l'attente");
+					}
+
+					// On affiche l'inter stimulation
+					showImageFromName(media.getInterStim().getName());
+
+					// On attend pendant la duration de l'inter stimulation
+
+					// wait x ?
+					try
+					{
+						TimeUnit.SECONDS.sleep(media.getInterStim().getDuration().getSeconds());
+					}
+					// Si l'attente est interrompue on le signale
+					catch (InterruptedException error)
+					{
+						System.out.println("Interruption de l'attente");
+					}
+
+				}
+				// Si le média est une vidéo
+				else if (media.getType() == TypeMedia.VIDEO)
+				{
+					// On cache l'image s'il y en avait une
+					removeImage();
+					// On affiche la vidéo
+					showMediaFromName(media.getName());
+					// On attend pendant la duration du Media
+
+					// wait x ?
+					try
+					{
+						TimeUnit.SECONDS.sleep(media.getDuration().getSeconds());
+					}
+					// Si l'attente est interrompue on le signale
+					catch (InterruptedException error)
+					{
+						System.out.println("Interruption de l'attente");
+					}
+				}
+
+			}
+		}
+		/*
+		mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.setAutoPlay(true);
+		mediaView.setMediaPlayer(mediaPlayer);
+		*/
+
 	}
 
 	/**
