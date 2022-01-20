3d2ddf5bb3a5c17bdac9c43aa0ab8b47a5257d2f
Mariem-abdelmalik <19059@esp.mr>
2022-01-20 20:53:24 +0100

    les testes unitaires

--- src/test/java/fr/polytech/circus/model/DataCircusTest.java
+++ src/test/java/fr/polytech/circus/model/DataCircusTest.java
@@ -1,5 +1,7 @@
 package fr.polytech.circus.model;
 
+import com.itextpdf.text.Meta;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationText;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,26 +17,60 @@ public class DataCircusTest implements Serializable {
     @BeforeEach
     void setUp() {
     }
-
     @Test
-    void getLieuxList() {
-    }
+    void getLieuxList1() {
+        DataCircus dataCircus1 = new DataCircus();
+        List<String> lieuxList1 = new ArrayList<>();
+        lieuxList1.add("lieu1");
+        lieuxList1.add("lieu1");
+        dataCircus1.setLieuxList(lieuxList1);
+        assertEquals(lieuxList1,dataCircus1.getLieuxList());
 
-    @Test
-    void getlocationsList (){
     }
-
     @Test
     void setLieuxList() {
+        DataCircus dataCircus1 = new DataCircus();
+        List<String> lieuxList = new ArrayList<>();
+        lieuxList.add("lieu");
+        dataCircus1.setLieuxList(lieuxList);
+        assertEquals(lieuxList,dataCircus1.getLieuxList());
+
+    }
+    @Test
+    void getMetaSequenceList() {
+        MetaSequence metaSequence1 = new MetaSequence("metaSeq1");
+        MetaSequence metaSequence2 = new MetaSequence("metaSeq2");
+        List<MetaSequence> metaSequences = new ArrayList<>();
+        metaSequences.add(metaSequence1);
+        metaSequences.add(metaSequence2);
+        DataCircus dataCircus = new DataCircus();
+        dataCircus.setMetaSequenceList(metaSequences);
+        assertEquals(metaSequences,dataCircus.getMetaSequenceList());
+
     }
 
     @Test
-    void getMetaSequenceList() {
-    }
+    void setLieuxList2(){
+        DataCircus dataCircus1 = new DataCircus();
+        List<String> lieuxList = new ArrayList<>();
+        dataCircus1.setLieuxList(lieuxList);
+        dataCircus1.setLieuxList("lieu2");
 
+        assertEquals(true,dataCircus1.getLieuxList().contains("lieu2"));
+
+
+    }
 
     @Test
     void setMetaSequenceList() {
+        MetaSequence metaSequence1 = new MetaSequence("metaSeq1");
+        MetaSequence metaSequence2 = new MetaSequence("metaSeq2");
+        List<MetaSequence> metaSequences = new ArrayList<>();
+        metaSequences.add(metaSequence1);
+        metaSequences.add(metaSequence2);
+        DataCircus dataCircus = new DataCircus();
+        dataCircus.setMetaSequenceList(metaSequences);
+        assertEquals(metaSequences,dataCircus.getMetaSequenceList());
     }
 
     @Test
--- src/test/java/fr/polytech/circus/model/MetaSequenceTest.java
+++ src/test/java/fr/polytech/circus/model/MetaSequenceTest.java
@@ -1,7 +1,13 @@
 package fr.polytech.circus.model;
 
+import fr.polytech.circus.CircusApplication;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,42 +18,113 @@ class MetaSequenceTest {
     }
 
     @Test
-    void getName() {
+    void getName1() {
+        MetaSequence metaSequence = new MetaSequence();
+        assertEquals("MetaSequence",metaSequence.getName());
+
     }
 
     @Test
-    void setName() {
+    void setName2() {
+        MetaSequence metaSequence = new MetaSequence();
+        metaSequence.setName("meta seq test");
+        assertEquals("meta seq test",metaSequence.getName());
+
     }
 
     @Test
     void getDuration() {
+        MetaSequence metaSequence = new MetaSequence();
+        assertEquals(Duration.ZERO,metaSequence.getDuration());
     }
 
     @Test
     void setDuration() {
+        MetaSequence metaSequence = new MetaSequence();
+        metaSequence.setDuration(Duration.ofSeconds(2));
+        assertEquals(Duration.ofSeconds(2),metaSequence.getDuration());
+
     }
 
     @Test
-    void getListSequences() {
+    void getListSequences1() {
+
+        MetaSequence metaSequence = new MetaSequence();
+        List<Sequence> sequences = new ArrayList<>();
+
+        Sequence sequence1 = new Sequence("seq 1");
+        Sequence sequence2 = new Sequence("seq 2");
+
+        sequences.add(sequence1);
+        sequences.add(sequence2);
+
+        metaSequence.setListSequences(sequences);
+        assertEquals(sequences,metaSequence.getListSequences());
+
     }
 
     @Test
-    void setListSequences() {
+    void setListSequences1() {
+
+        MetaSequence metaSequence = new MetaSequence();
+        List<Sequence> sequences = new ArrayList<>();
+
+        Sequence sequence1 = new Sequence("seq 1");
+        Sequence sequence2 = new Sequence("seq 2");
+
+        sequences.add(sequence1);
+        sequences.add(sequence2);
+
+        metaSequence.setListSequences(sequences);
+        assertEquals(sequences,metaSequence.getListSequences());
+
     }
 
     @Test
     void testEquals() {
-    }
+        MetaSequence metaSequence1 = new MetaSequence("metaSeq1");
+
+        MetaSequence metaSequence2 = new MetaSequence("metaSeq1");
+
+        Assert.assertEquals(true,metaSequence2.equals(metaSequence1));
+        
+           }
 
     @Test
     void testToString() {
+        MetaSequence metaSequence1 = new MetaSequence("metaSeq1");
+        Assert.assertEquals("metaSeq1",metaSequence1.toString());
+
+    }
+    @Test
+    void testToString2() {
+        MetaSequence metaSequence1 = new MetaSequence("");
+        Assert.assertEquals("",metaSequence1.toString());
+
     }
 
     @Test
     void addSequence() {
+
+        Sequence sequence1 = new Sequence("seq 1");
+        sequence1.setDuration(Duration.ofSeconds(1));
+
+        MetaSequence metaSequence = new MetaSequence("metaSeq 1");
+        metaSequence.setDuration(Duration.ofSeconds(2));
+        metaSequence.addSequence(sequence1);
+        Assert.assertEquals(true,metaSequence.getListSequences().contains(sequence1));
+
     }
 
     @Test
     void remSequence() {
+        Sequence sequence1 = new Sequence("seq 1");
+        sequence1.setDuration(Duration.ofSeconds(1));
+
+        MetaSequence metaSequence = new MetaSequence("metaSeq 1");
+        metaSequence.setDuration(Duration.ofSeconds(2));
+        metaSequence.addSequence(sequence1);
+        metaSequence.remSequence(sequence1);
+        Assert.assertEquals(false,metaSequence.getListSequences().contains(sequence1));
     }
 }
